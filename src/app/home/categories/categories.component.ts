4764555e7907ca807d98209fb90a10a17f77dddb
muhammadhashir153 <hashir.mehtab1503@gmail.com>
2025-01-01 18:49:53 +0500

    Landing update v1.0

--- src/app/home/categories/categories.component.ts
+++ src/app/home/categories/categories.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CategoryService } from 'src/app/APISERVICES/Categoryservice';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
-export class CategoriesComponent {
 
+export class CategoriesComponent {
+  constructor(private categoryService: CategoryService){}
+
+  categories: any[] = [];
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(response => {
+      this.categories = response;
+    });
+  }
 }
