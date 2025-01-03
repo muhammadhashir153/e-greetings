15cfb48b1235ed8a04930ada83619932c0e00c39
muhammadhashir153 <hashir.mehtab1503@gmail.com>
2025-01-03 18:32:37 +0500

    final header😂

--- src/app/home/tempelates/tempelates.component.ts
+++ src/app/home/tempelates/tempelates.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TempService } from 'src/app/APISERVICES/TempService';
 import { NgFor } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from 'src/app/APISERVICES/CategoryService';
 
 @Component({
   selector: 'app-tempelates',
@@ -11,9 +12,11 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './tempelates.component.scss'
 })
 export class TempelatesComponent {
-  constructor(private tempService: TempService, private activeRoute :ActivatedRoute) { }
+  constructor(private tempService: TempService, private catService: CategoryService, private activeRoute :ActivatedRoute) { }
 
   id :string | null = null;
+  categoryName: string | null = null;
+  catDesc: string | null = null;
   categories :any[] = [];
   templates :any[] = [];
 
@@ -23,10 +26,16 @@ export class TempelatesComponent {
 
     this.tempService.getService().subscribe(response => {
       this.categories = response;
-
       this.templates = this.categories.filter(category => category.categoryId == this.id);
-
-      console.log(this.templates);
     });
+
+    this.catService.getCategories().subscribe(res => {
+      this.categories = res;
+      this.categories.filter(name => name.id == this.id);
+      this.categoryName = this.categories[0].name;
+      this.catDesc = this.categories[0].description;
+      console.log(this.categoryName);
+      console.log(this.catDesc);
+    })
   }
 }
